f69fa97c9032fd92e642ac6ffa8308a2b954f233
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-02-19 17:01:16 -0800

    installed react-icons pkg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import styles from '@/styles/Home.module.css'
-
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillMediumSquare } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 export default function Footer() {
 
@@ -7,9 +9,9 @@ export default function Footer() {
         <div className="footer">
             <p className={styles.details}>Gary Stroup &#169; 2023</p>
             <div className={styles.flex}>
-                <a href="https://medium.com/@stroup.uxuidesign">Medium  </a>
-                <a href="https://www.linkedin.com/in/gary-stroup-2317a247">LinkedIn </a>
-                <a href="https://www.instagram.com/gary.codez/">Instagram </a>  
+                <a href="https://medium.com/@stroup.uxuidesign"><AiFillMediumSquare className={styles.icons} /> </a>
+                <a href="https://www.linkedin.com/in/gary-stroup-2317a247"><AiFillLinkedin className={styles.icons} /></a>
+                <a href="https://www.instagram.com/gary.codez/"><AiOutlineInstagram className={styles.icons} /></a>  
             </div>
             
         </div>
