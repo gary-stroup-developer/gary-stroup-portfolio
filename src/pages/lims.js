efede15289114262ab05eaf7934e3cbcb025e339
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-05-29 21:19:49 -0700

    updated projects and lims pages. Modified styling for both so that everything looks presentable for responsiveness

--- src/pages/lims.js
+++ src/pages/lims.js
@@ -9,8 +9,27 @@ const lims = () => {
       <Link className='link' href="/projects">  Projects </Link>
       <h1 className={styles.title}>Job Management Database</h1>
       <div className='container'>
+         <p className={styles.projectSummary}>This project was built to address user experience feedback on a newly designed web
+            application for my department at work. The web application created by my company was modeled
+            after our current Lotus Notes LIMS desktop application. The feedback was gathered from 3 interviews with employers.
+            The main concerns expressed by the users was the lack of functionality amongst the features offerred,
+            poor user interface, and difficulty navigating the application to find information.
+          </p>
+          <p className={styles.projectSummary}>
+            I built this application to be user friendly. My main focus was to create a centralized location where
+            the information users need can be accessed quickly. To accomplish this feature, I created a dashboard page that users 
+            are routed to once login is successful. The dashboard has a side navigation that is split between two sections, the Users 
+            and the Admin. The Users section is available to all users in the department, which features links to all forms and data they
+            would need to access. The Admin section is only displayed if the user logged in has admin privileges.
+          </p>
+          <p className={styles.projectSummary}>
+            The main section displays tiles that pertain to each user. The tile displays the name, current job capacity, and a link to view their
+            jobs currently in production. Clicking the link routes the user to the user dashboard where they can view their open jobs, create a new job,
+            or update their capacity by dragging and dropping the job cards into a different section of the workflow. The feedback I received after demonstration 
+            was positive. Everyone enjoyed how everything was centralized and the app was easy to navigate. 
+          </p>
         <section>
-          <div className='flex-container'> 
+          <div className='flex-container margin'> 
             <Image
               className={styles.projectImage}
               src="/images/login.svg"
@@ -20,11 +39,12 @@ const lims = () => {
             />
             <p className={`${styles.ImageSummaryTop} ${styles.ImageSummary}`}>
               This is the login screen of the application. Credentials stored and accessed in MongoDB. The backend used for this 
-              project was Golang.
+              project was Golang. The backend determines whether the user is an admin, regular department user, or outside department 
+              user. 
             </p>
           </div>
-          <div>
-            
+
+          <div className='flex-container margin'>
             <Image
               className={styles.projectImage}
               src="/images/dashboardPage.svg"
@@ -33,10 +53,13 @@ const lims = () => {
               height={300}
             />
             <p className={`${styles.ImageSummaryTop} ${styles.ImageSummary}`}>
-              This is the dashboard page where users can access forms or view their workflow.
+              This is the dashboard page where users can access forms in the side naviagtion. The main section of the dashboard
+              displays employee tiles that displays the name, current capacity, and a link to view their workflow. The key highlight 
+              of the dashboard is that it is the central location of the app where all features are routed from. This addresses the confusion 
+              of the user navigating the current live application.
             </p>
           </div>
-          <div className='flex-container'>
+          <div className='flex-container margin'>
             <Image
               className={styles.projectImage}
               src="/images/createNewJobRequest.svg"
@@ -50,7 +73,7 @@ const lims = () => {
               with the respective information.
             </p>
           </div>
-          <div className='flex-container'>
+          <div className='flex-container margin'>
             <Image
               className={styles.projectImage}
               src="/images/capacity.svg"
@@ -59,12 +82,12 @@ const lims = () => {
               height={300}
             />
             <p className={styles.ImageSummary}>
-              This image shows the current capacity. Below, the capacity will be updated and the user is notified by a message in the top 
+              The image above shows the current capacity at 15.38%. Below, the capacity was updated to 0.00% and the user is notified by a message in the top 
               left corner of the screen. I decided to build the workflow like a kanban system, where the user can move the job through the 
-              different stages of the production cycle. Video demonstration can be found on my instagram.
+              different stages of the production cycle. Video demonstration can be found on my instagram (link in footer).
             </p>
           </div>
-          <div className='flex-container gap'>
+          <div className='flex-container margin'>
             <Image
               className={styles.projectImage}
               src="/images/updatedCapacity.svg"
